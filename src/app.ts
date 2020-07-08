2965a24c2ce6999eace3ce57221918f81238f710
Mars073 <imars073@live.be>
2020-07-08 16:22:40 +0200

    serverless text + add custom loader

--- src/app.ts
+++ src/app.ts
@@ -3,25 +3,20 @@ import dotenv from 'dotenv'
 import express from 'express'
 import bodyParser from 'body-parser'
 import compression from 'compression'
-import getInfo from './controllers/getInfo'
+import cLoader from './controllers/Loader'
 
 dotenv.config()
 
 const app = express()
-// lesconst port = process.env['SERVER_PORT'] || 3002
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(compression())
 
-app.get('/ping', function (request, response) {
+app.get('/.netlify/functions/app/ping', function (request, response) {
   response.send('pong')
 })
 
-app.use('/getInfo', getInfo)
+cLoader.loadFor(app, '/.netlify/functions/app')
 
-/* app.listen(port, function () {
-  console.log(`Server listening on port ${ port }!🚀`)
-}); */
-
-module.exports.handler = serverless(app)
+exports.handler = serverless(app)
