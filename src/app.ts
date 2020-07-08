20ff2f1a5af10a56df10c739a113b271ea65e4e0
Mars073 <imars073@live.be>
2020-07-08 15:03:21 +0200

    serverless test

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,27 @@
 import serverless from 'serverless-http'
 import dotenv from 'dotenv'
 import express from 'express'
+import bodyParser from 'body-parser'
+import compression from 'compression'
 import getInfo from './controllers/getInfo'
 
 dotenv.config()
 
 const app = express()
-const port = process.env['SERVER_PORT'] || 3002
+// const port = process.env['SERVER_PORT'] || 3002
 
-app.use(express.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+app.use(compression())
+
+app.get('/ping', function (request, response) {
+  response.send('pong')
+})
 
 app.use('/getInfo', getInfo)
 
-app.listen(port, function () {
+/* app.listen(port, function () {
   console.log(`Server listening on port ${ port }!🚀`)
-});
+}); */
 
 module.exports.handler = serverless(app)
