4e9b9802a34b16b02de83a40e3ca3d861770cc36
Mars073 <imars073@live.be>
2020-07-20 17:57:59 +0200

    rename random search param

--- src/facades/search/index.ts
+++ src/facades/search/index.ts
@@ -3,42 +3,42 @@ import Cache from '../../utils/GISCache'
 import AppError from "./../../errors/AppError";
 
 export interface Request {
-    lang: 'fr' | 'nl';
-    query: string;
+  lang: 'fr' | 'nl';
+  query: string;
 }
 
 interface Result {
-    id: string;
-    name: string;
+  id: string;
+  name: string;
 }
 
 export interface Response //  extends Building
 {
-    lang: 'fr' | 'nl';
-    buildings: Result[];
-    cities: Result[];
-    intervenants: Result[];
-    streets: Result[];
-    styles: Result[];
-    typos: Result[];
+  lang: 'fr' | 'nl';
+  buildings: Result[];
+  cities: Result[];
+  intervenants: Result[];
+  streets: Result[];
+  styles: Result[];
+  typos: Result[];
 }
 
 @Handler(Search.Type)
 export class Search implements ICommandHandler<Request, Response> {
-    public static get Type (): string { return 'Search' }
+  public static get Type (): string { return 'Search' }
 
-    Validate (request: Request): void {
-        if (typeof request.query !== 'string' || request.query.length < 2) {
-            throw new AppError(400, "Query too short")
-        }
+  Validate (request: Request): void {
+    if (typeof request.query !== 'string' || request.query.length < 2) {
+      throw new AppError(400, "Query too short")
     }
+  }
 
-    Handle (command: Request): Response {
-        command.query = '%' + command.query.trim() + '%'
+  Handle (command: Request): Response {
+    command.query = '%' + command.query.trim() + '%'
 
-        const stmt_buildings = Cache.context.prepare(``)
-        const stmt_cities = Cache.context.prepare(``)
-        const stmt_inter = Cache.context.prepare(`
+    const stmt_buildings = Cache.context.prepare(``)
+    const stmt_cities = Cache.context.prepare(``)
+    const stmt_inter = Cache.context.prepare(`
       SELECT
         uuid as id,
         name
@@ -46,8 +46,8 @@ export class Search implements ICommandHandler<Request, Response> {
       WHERE
         name LIKE ?
     `)
-        const stmt_styles = Cache.context.prepare(``);
-        const stmt_street = Cache.context.prepare(`
+    const stmt_styles = Cache.context.prepare(``);
+    const stmt_street = Cache.context.prepare(`
       SELECT
         uuid as id,
         name_${command.lang} as name
@@ -57,16 +57,16 @@ export class Search implements ICommandHandler<Request, Response> {
           OR
         (name_nl LIKE ?)
     `)
-        const stmt_typos = Cache.context.prepare(``)
+    const stmt_typos = Cache.context.prepare(``)
 
-        return {
-            lang: command.lang,
-            cities: stmt_cities.all(command.query),
-            buildings: stmt_buildings.all(command.query),
-            intervenants: stmt_inter.all(command.query),
-            streets: stmt_street.all(command.query, command.query),
-            styles: stmt_styles.all(command.query),
-            typos: stmt_typos.all(command.query)
-        } as Response
-    }
+    return {
+      lang: command.lang,
+      cities: stmt_cities.all(command.query),
+      buildings: stmt_buildings.all(command.query),
+      intervenants: stmt_inter.all(command.query),
+      streets: stmt_street.all(command.query, command.query),
+      styles: stmt_styles.all(command.query),
+      typos: stmt_typos.all(command.query)
+    } as Response
+  }
 }
--- src/facades/search/random/index.ts
+++ src/facades/search/random/index.ts
@@ -1,55 +1,55 @@
-import {ICommandHandler, Handler} from 'tsmediator'
+import { ICommandHandler, Handler } from 'tsmediator'
 import Cache from '../../../utils/GISCache'
 import AppError from "./../../../errors/AppError";
 
 export interface Request {
-    lang: 'fr' | 'nl';
-    query: number;
+  lang: 'fr' | 'nl';
+  limit: number;
 }
 
 interface Result {
-    id: string;
-    name: string;
-    building: string;
-    city: string;
-    intervenant: unknown[];
-    street: string;
-    styles: unknown[];
-    typos: string;
+  id: string;
+  name: string;
+  building: string;
+  city: string;
+  intervenant: unknown[];
+  street: string;
+  styles: unknown[];
+  typos: string;
 }
 
 export interface Response {
-    lang: 'fr' | 'nl';
-    result: Result[];
+  lang: 'fr' | 'nl';
+  result: Result[];
 }
 
 @Handler(SearchRandom.Type)
 export class SearchRandom implements ICommandHandler<Request, Response> {
-    public static get Type(): string {
-        return 'SearchRandom'
-    }
+  public static get Type(): string {
+    return 'SearchRandom'
+  }
 
-    Validate (request: Request): void {
-        if (isNaN(request.query)) {
-            throw new AppError(400, "Expecting an amount")
-        }
+  Validate (request: Request): void {
+    if (isNaN(request.limit)) {
+      throw new AppError(400, "Expecting an amount")
     }
+  }
 
-    Handle(command: Request): Response {
-        console.log(command)
-        const stmt_facts = Cache.context.prepare(`
+  Handle (command: Request): Response {
+    console.log(command)
+    const stmt_facts = Cache.context.prepare(`
           SELECT
             name_${command.lang} as name
           FROM buildings
           WHERE
             (name_nl and name_fr) is not null
           ORDER BY random()
-          LIMIT ${command.query}
+          LIMIT ?
         `)
 
-        return {
-            lang: command.lang,
-            result: stmt_facts.all()
-        } as Response
-    }
+    return {
+      lang: command.lang,
+      result: stmt_facts.all(command.limit)
+    } as Response
+  }
 }
