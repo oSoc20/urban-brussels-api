680330c7e0cd6397b932dc317a6b06a35154ccb1
Mars073 <imars073@live.be>
2020-07-22 11:24:17 +0200

    Add Buildings per style

---
+++ src/facades/stats/BuildingsPerStyle.ts
@@ -0,0 +1,36 @@
+import { ICommandHandler, Handler } from 'tsmediator'
+import Cache from '../../utils/GISCache'
+
+export interface Request {
+  lang: 'fr' | 'nl';
+}
+
+export declare type Response = { [key: string]: number }
+
+@Handler(BuildingsPerStyle.Type)
+export class BuildingsPerStyle implements ICommandHandler<Request, Response> {
+  public static get Type (): string { return 'BuildingsPerStyle' }
+
+
+  Handle (command: Request): Response {
+
+    const stmt = Cache.context.prepare(`
+      SELECT
+        styles.name_${command.lang} as name,
+        COUNT(*) as building_count
+      FROM
+        styles
+      JOIN
+        buildings_styles
+        ON (styles.uuid = buildings_styles.style_id)
+      GROUP BY
+        styles.uuid
+    `)
+
+    const response = {} as Response
+
+    stmt.all().forEach(i => response[i.name] = i.building_count)
+
+    return response
+  }
+}
--- src/facades/stats/index.ts
+++ src/facades/stats/index.ts
@@ -1,6 +1,7 @@
 import { ICommandHandler, Handler, Mediator } from 'tsmediator'
 import { Response as ResponseSPC, StylesPerCity } from './StylesPerCity'
 import { Response as ResponseBPI, BuildingsPerIntervenant } from './BuildingsPerIntervenant'
+import { Response as ResponseBPS, BuildingsPerStyle } from './BuildingsPerStyle'
 
 export interface Request {
   lang: 'fr' | 'nl';
@@ -12,6 +13,7 @@ export interface Response
   lang: 'fr' | 'nl';
   stylesPerCity: ResponseSPC;
   BuildingsPerIntervenant: ResponseBPI;
+  BuildingsPerStyle: ResponseBPS;
 }
 
 const mediator = new Mediator()
@@ -26,7 +28,8 @@ export class GlobalStats implements ICommandHandler<Request, Response> {
     return {
       lang: command.lang,
       stylesPerCity: mediator.Send(StylesPerCity.Type, command),
-      BuildingsPerIntervenant: mediator.Send(BuildingsPerIntervenant.Type, command)
+      BuildingsPerIntervenant: mediator.Send(BuildingsPerIntervenant.Type, command),
+      BuildingsPerStyle: mediator.Send(BuildingsPerStyle.Type, command)
     } as Response
   }
 }
