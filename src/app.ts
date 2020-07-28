0c55f388bcffd5470fefba08b6753cea6995b1ad
Mars073 <imars073@live.be>
2020-07-28 16:09:53 +0200

    remove useless code + doc

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,6 @@ import cors from 'cors'
 import bodyParser from 'body-parser'
 import compression from 'compression'
 import errorHandler from './errors/handler'
-import getInfo from './controllers/getInfo'
 import getAutocomplete from './controllers/autocomplete'
 import getStatistics from './controllers/stats'
 import getSearch from './controllers/search'
@@ -16,31 +15,38 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(compression())
 app.use(cors())
-app.use(function (request, _response, next) {
-  // prevent sql injection with "lang"
-  if (typeof request.query.lang !== 'string' || !request.query.lang.match(/^(fr|nl)$/)) {
-    if (request.headers['accept-language'] && request.headers['accept-language'].match(/nl/)) {
-      request.query.lang = 'nl'
-    } else {
-      request.query.lang = 'fr'
+app.use(
+  /**
+   * Force `lang` declaration and prevent sql injection
+   * 
+   * @param request - client request
+   * @param _response - server response
+   * @param next - next handler callback
+   */
+  function (request, _response, next) {
+    if (typeof request.query.lang !== 'string' || !request.query.lang.match(/^(fr|nl)$/)) {
+      if (request.headers['accept-language'] && request.headers['accept-language'].match(/nl/)) {
+        request.query.lang = 'nl'
+      } else {
+        request.query.lang = 'fr'
+      }
     }
-  }
-  if (typeof request.body.lang !== 'string' || !request.body.lang.match(/^(fr|nl)$/)) {
-    if (request.headers['accept-language'] && request.headers['accept-language'].match(/nl/)) {
-      request.body.lang = 'nl'
-    } else {
-      request.body.lang = 'fr'
+    if (typeof request.body.lang !== 'string' || !request.body.lang.match(/^(fr|nl)$/)) {
+      if (request.headers['accept-language'] && request.headers['accept-language'].match(/nl/)) {
+        request.body.lang = 'nl'
+      } else {
+        request.body.lang = 'fr'
+      }
     }
+    next()
   }
-  next()
-})
+)
 
 app.get('/ping', function (_request, response) {
   response.send('pong')
 })
 
 // controllers
-app.use('/info', getInfo)
 app.use('/search', getSearch)
 app.use('/autocomplete', getAutocomplete)
 app.use('/stats', getStatistics)
